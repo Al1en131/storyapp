d8e44a3982333be4b9871e6e6679b7515daa651d
Alif Essa Nurcahyani <essanurcahyania@gmail.com>
2025-04-30 10:01:52 +0700

    update

--- docs/sw.js
+++ docs/sw.js
@@ -1,12 +1,12 @@
 const CACHE_NAME = 'story-app-cache-v1';
 const urlsToCache = [
     '/',
-    'index.html',
+    '/index.html',
     '/scripts/index.js',
     '/scripts/pages/story/storyPage.js',
     '/scripts/presenters/storyPresenter.js',
     '/scripts/utils/indexedDB.js',
-    'styles/main.css'
+    '/styles/main.css'
 ];
 
 self.addEventListener('install', event => {
